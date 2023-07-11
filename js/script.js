fa08ba94cea08d02d1100509b35012de4ebc1f07
Sudip78 <139178919+Sudip78@users.noreply.github.com>
2023-07-11 22:32:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+// banner
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+
+
+
